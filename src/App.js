5a5e5fd24c62c52a9ab555dc8f3ec16927f780a3
Qu33nB <bdcotton52@gmail.com>
2020-07-01 18:02:24 -0500

    Updated the codebase

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      shoes: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://127.0.0.1:8000/api/shoe/')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      this.setState({shoes: data})
+    })
+  }
+
+  handleLikes(props) {
+    console.log(props)
+  }
+  
+  render() {
+      return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Qu33n B's Shoe Store</h1>
+          <ul>
+            <p>Shoe Brand/Size</p>
+            {this.state.shoes.map(p => {
+              console.log(p)
+              return (<li>{p.brand}/{p.size}</li>
+                        <p>{p.manufacturer</p>)
+            })}
+          </ul>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
